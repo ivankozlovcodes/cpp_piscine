a3f53a13a9d05c5bacae846bf72370dace90a9aa
Ivan Kozlov <ikozlov9000@gmail.com>
2018-06-30 17:09:36 -0700

    d05 ex00

--- d05/ex00/Bureaucrat.hpp
+++ d05/ex00/Bureaucrat.hpp
@@ -6,14 +6,14 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/30 15:29:20 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/06/30 16:11:18 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/06/30 17:06:19 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
-# include <string>
+# include <iostream>
 # include <exception>
 
 # define MIN_GRADE 1
@@ -25,24 +25,52 @@
 class	Bureaucrat
 {
 	public:
+
+		/*
+		 *	GradeTooHighException class
+		 */
 		class GradeTooHighException : public std::exception
 		{
 			public:
 				GradeTooHighException(void);
+				GradeTooHighException(const GradeTooHighException &src);
 				virtual ~GradeTooHighException(void) throw();
+
+				GradeTooHighException &operator= (const GradeTooHighException &obj);
 
 				virtual const char	*what() const throw();
 		};
 
+		/*
+		 *	GradeTooLowException class
+		 */
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				GradeTooLowException(void);
+				GradeTooLowException(const GradeTooLowException &src);
+				virtual ~GradeTooLowException(void) throw();
+
+				GradeTooLowException &operator= (const GradeTooLowException &obj);
+
+				virtual const char	*what() const throw();
+		};
+
+		Bureaucrat(void);
+		Bureaucrat(const Bureaucrat &src);
 		Bureaucrat(std::string name, int grade);
 		~Bureaucrat(void);
 
-		int				getGrade(void);
-		std::string		getName(void);
+		Bureaucrat				&operator= (const Bureaucrat &src);
+		friend std::ostream		&operator<< (std::ostream &out,\
+				const Bureaucrat &obj);
 
-		void			setName(std::string name);
-		void			incGrade(int value);
-		void			decGrade(int value);
+		int						getGrade(void) const;
+		std::string				getName(void) const;
+
+		void					setName(std::string name);
+		void					incGrade(void);
+		void					decGrade(void);
 
 	private:
 		int				_grade;
--- d05/ex00/main.cpp
+++ d05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/30 15:55:11 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/06/30 16:09:05 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/06/30 17:01:04 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,33 @@
 
 #include "Bureaucrat.hpp"
 
+static void
+checkBureaucrat(Bureaucrat test, int min, int max)
+{
+	try
+	{
+		for(int i = min; i <= max; i++)
+		{
+			// test.incGrade();
+			test.decGrade();
+			// std::cout << test << std::endl;
+		}
+	}
+	catch (std::exception & e)
+	{
+		std::cout << test;
+		std::cout << e.what() << std::endl;
+	}
+	return ;
+}
+
 int
 main(void)
 {
-    Bureaucrat  asd("Asd", 10);
+	Bureaucrat  asd("Asd", 10);
 
-    std::cout << asd.getGrade() << std::endl;
-    std::cout << asd.getName() << std::endl;
-    asd.incGrade(150);
-    return (0);
+	std::cout << asd.getGrade() << std::endl;
+	std::cout << asd.getName() << std::endl;
+	checkBureaucrat(asd, 0, 150);
+	return (0);
 }
