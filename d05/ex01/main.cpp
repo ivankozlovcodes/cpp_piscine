a2aea56ae2ddf4c5042760ac620e8300526e5725
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-06-30 20:24:07 -0700

    Added tests to main

--- d05/ex01/main.cpp
+++ d05/ex01/main.cpp
@@ -3,32 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/30 15:55:11 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/06/30 17:01:04 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/06/30 20:08:55 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <stdexcept>
 
+#include "Form.hpp"
 #include "Bureaucrat.hpp"
 
 static void
-checkBureaucrat(Bureaucrat test, int min, int max)
+checkForm(Bureaucrat bureaucrat, Form format)
 {
 	try
 	{
-		for(int i = min; i <= max; i++)
-		{
-			// test.incGrade();
-			test.decGrade();
-			// std::cout << test << std::endl;
-		}
+		bureaucrat.signForm(format);
+		std::cout << bureaucrat << std::endl;
+		std::cout << format << std::endl;
 	}
-	catch (std::exception & e)
+	catch (std::exception &e)
 	{
-		std::cout << test;
 		std::cout << e.what() << std::endl;
 	}
 	return ;
@@ -37,10 +35,21 @@ checkBureaucrat(Bureaucrat test, int min, int max)
 int
 main(void)
 {
-	Bureaucrat  asd("Asd", 10);
+	Form        f1 = Form("A form", 42, 42);
+	Form        f2 = Form("Another one...", 30, 50);
+	Bureaucrat  b1 = Bureaucrat("Marvin", 42);
+	Bureaucrat  b2 = Bureaucrat("Mr. Abraham", 1);
 
-	std::cout << asd.getGrade() << std::endl;
-	std::cout << asd.getName() << std::endl;
-	checkBureaucrat(asd, 0, 150);
+	checkForm(b1, f1);
+	std::cout << std::endl;
+
+	checkForm(b1, f2);
+	std::cout << std::endl;
+
+	checkForm(b2, f1);
+	std::cout << std::endl;
+
+	checkForm(b2, f2);
+	std::cout << std::endl;
 	return (0);
 }
