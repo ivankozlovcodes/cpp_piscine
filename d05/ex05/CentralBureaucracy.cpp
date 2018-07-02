decf48309c1e31a080862f79c36e0e76a24303f4
Ivan Kozlov <ikozlov9000@gmail.com>
2018-07-01 19:22:03 -0700

    Added central bureaucracy

---
+++ d05/ex05/CentralBureaucracy.cpp
@@ -0,0 +1,179 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CentralBureaucracy.cpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/01 18:30:47 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/07/01 19:17:07 by ikozlov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "CentralBureaucracy.hpp"
+#include "CentralBureaucracy.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+CentralBureaucracy::CentralBureaucracy(void)
+{
+	srand(time(0));
+	_generateInterns();
+	return;
+}
+
+CentralBureaucracy::CentralBureaucracy(CentralBureaucracy &src)
+{
+	*this = src;
+}
+
+CentralBureaucracy::~CentralBureaucracy(void) { }
+
+void
+CentralBureaucracy::_generateInterns(void)
+{
+	Intern whocares;
+
+	for (int i = 0; i < _blocks_num; i++)
+		_blocks[i].setIntern(whocares);
+}
+
+CentralBureaucracy
+&CentralBureaucracy::operator=(const CentralBureaucracy &src) 
+{
+	std::copy(src._targets,\
+		src._targets + CentralBureaucracy::_targets_num, this->_targets);
+	// std::copy(src._blocks,\
+		src._blocks + CentralBureaucracy::_blocks_num, this->_blocks);
+	return (*this);
+}
+
+void
+CentralBureaucracy::doBureaucracy(void)
+{
+	int				x;
+	std::string		forms[] = { PP, RR, SC };
+
+	for (int i = 0; i < CentralBureaucracy::_targets_num; i++)
+	{
+		if (_targets[i] == "")
+			break;
+		std::cout << std::endl;
+		while (_targets[i] != "")
+		{
+			x = rand() % 20;
+			try
+			{
+				_blocks[x].doBureaucracy(forms[x % 3], _targets[i]);
+			}
+			catch (std::exception &e)
+			{
+				std::cout << "Can't execute form :(" << std::endl;
+				std::cout << e.what() << std::endl;
+				continue;
+			}
+			_targets[i] = "";
+		}
+	}
+}
+
+void
+CentralBureaucracy::feedSigner(Bureaucrat &obj)
+{
+	for (int i = 0; i < CentralBureaucracy::_blocks_num; i++)
+		if (_blocks[i].getSigner() == NULL)
+		{
+			_blocks[i].setSigner(obj);
+			return ;
+		}
+	throw new CentralBureaucracy::TooManyEmployeesException();
+}
+
+void
+CentralBureaucracy::feedExecuter(Bureaucrat &obj)
+{
+	for (int i = 0; i < CentralBureaucracy::_blocks_num; i++)
+		if (_blocks[i].getExecutor() == NULL)
+		{
+			_blocks[i].setExecutor(obj);
+			return ;
+		}
+	throw new CentralBureaucracy::TooManyEmployeesException();
+}
+
+void	CentralBureaucracy::queueUp(std::string obj)
+{
+	for (int i = 0; i < CentralBureaucracy::_targets_num; i++)
+		if (_targets[i] == "")
+		{
+			_targets[i] = obj;
+			return ;
+		}
+	throw new CentralBureaucracy::TooBusyException();
+}
+
+/*
+** TooBusyException Nested Class
+*/
+
+const char
+*CentralBureaucracy::TooBusyException::message = "CentralBureaucracy Error:\
+		Too busy to accept new forms!";
+
+
+CentralBureaucracy::TooBusyException::TooBusyException(void) { }
+
+CentralBureaucracy::TooBusyException::TooBusyException(TooBusyException const &obj)
+{
+	*this = obj;
+}
+
+CentralBureaucracy::TooBusyException::~TooBusyException(void) throw() { }
+
+CentralBureaucracy::TooBusyException
+&CentralBureaucracy::TooBusyException::operator=(TooBusyException const &r) 
+{
+	(void)r;
+	return (*this);
+}
+
+const char*
+CentralBureaucracy::TooBusyException::what() const throw()
+{
+	return (CentralBureaucracy::TooBusyException::message);
+}
+
+/*
+** TTooManyEmployeesException Nested Class
+*/
+
+const char
+*CentralBureaucracy::TooManyEmployeesException::message = "CentralBureaucracy Error:\
+		We are overstuffed already adding new employees won't help!";
+
+CentralBureaucracy::TooManyEmployeesException::\
+TooManyEmployeesException(void) { }
+
+CentralBureaucracy::TooManyEmployeesException::\
+TooManyEmployeesException(TooManyEmployeesException const &obj)
+{
+	*this = obj;
+}
+
+CentralBureaucracy::TooManyEmployeesException::~TooManyEmployeesException(void) throw() { }
+
+CentralBureaucracy::TooManyEmployeesException
+&CentralBureaucracy::TooManyEmployeesException::operator=(TooManyEmployeesException const &r) 
+{
+	(void)r;
+	return (*this);
+}
+
+const char*
+CentralBureaucracy::TooManyEmployeesException::what() const throw()
+{
+	return (CentralBureaucracy::TooManyEmployeesException::message);
+}
---
+++ d05/ex05/CentralBureaucracy.hpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CentralBureaucracy.hpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/01 18:24:34 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/07/01 19:04:40 by ikozlov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CENTRALBUREAUCRACY_H
+# define CENTRALBUREAUCRACY_H
+
+#include <iostream>
+#include "Form.hpp"
+#include "Intern.hpp"
+#include "Bureaucrat.hpp"
+#include "OfficeBlock.hpp"
+
+class	CentralBureaucracy
+{
+
+	public:
+
+		class TooBusyException : public std::exception
+		{
+			public:
+				
+				TooBusyException(void);
+				TooBusyException(TooBusyException const &obj);
+				virtual ~TooBusyException(void) throw();
+				TooBusyException &operator=(TooBusyException const &r);
+				virtual const char* what() const throw();
+
+				static const char	*message;
+		};
+
+		class TooManyEmployeesException : public std::exception
+		{
+			public:
+				
+				TooManyEmployeesException(void);
+				TooManyEmployeesException(TooManyEmployeesException const &obj);
+				virtual ~TooManyEmployeesException(void) throw();
+				TooManyEmployeesException &operator=(TooManyEmployeesException const &r);
+				virtual const char* what() const throw();
+
+				static const char	*message;
+		};	
+
+		CentralBureaucracy(void);
+		CentralBureaucracy(CentralBureaucracy &obj);
+		virtual ~CentralBureaucracy(void);
+		CentralBureaucracy &operator=(const CentralBureaucracy &src);
+
+		void	feedSigner(Bureaucrat &obj);
+		void	feedExecuter(Bureaucrat &obj);
+
+		void	queueUp(std::string obj);
+
+		void	doBureaucracy(void);
+
+	private:
+
+		static const int	_blocks_num = 20;
+		static const int	_targets_num = 100;
+
+		void				_generateInterns(void);
+
+		OfficeBlock			_blocks[CentralBureaucracy::_blocks_num];
+		std::string			_targets[CentralBureaucracy::_targets_num];
+}; 
+
+#endif
