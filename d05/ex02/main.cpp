a25861781dd0e1a3c1426a47d424bf24db2a19db
Ivan Kozlov <ikozlov9000@gmail.com>
2018-07-02 20:35:47 -0700

    Fixed random for robotomy

--- d05/ex02/main.cpp
+++ d05/ex02/main.cpp
@@ -6,10 +6,12 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/30 15:55:11 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/06/30 22:06:23 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/07/02 20:33:13 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <time.h>
+#include <stdlib.h>
 #include <iostream>
 #include <stdexcept>
 
@@ -22,40 +24,42 @@
 
 static void
 signAndExecuteForms(Bureaucrat b, ShrubberyCreationForm f1, \
-                    RobotomyRequestForm f2, PresidentialPardonForm f3)
+					RobotomyRequestForm f2, PresidentialPardonForm f3)
 {
-    try
-    {
-        b.signForm(f1);
-        b.signForm(f2);
-        b.signForm(f3);
+	try
+	{
+		b.signForm(f1);
+		b.signForm(f2);
+		b.signForm(f3);
 
-        b.executeForm(f1);
-        b.executeForm(f2);
-        b.executeForm(f3);
-    }
-    catch (std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    return ;
+		b.executeForm(f1);
+		b.executeForm(f2);
+		b.executeForm(f3);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return ;
 }
 
 int
 main(void)
 {
-    Bureaucrat                    b1("B1", 69);
-    Bureaucrat                    b2("B2", 42);
-    Bureaucrat                    b3("B3", 1);
-    ShrubberyCreationForm         f1("SCF_2");
-    RobotomyRequestForm           f2("RRF_2");
-    PresidentialPardonForm        f3("PDF_2");
+	srand(time(NULL));
 
-    signAndExecuteForms(b1, f1, f2, f3);
+	Bureaucrat                    b1("B1", 69);
+	Bureaucrat                    b2("B2", 42);
+	Bureaucrat                    b3("B3", 1);
+	ShrubberyCreationForm         f1("SCF_2");
+	RobotomyRequestForm           f2("RRF_2");
+	PresidentialPardonForm        f3("PDF_2");
 
-    signAndExecuteForms(b2, f1, f2, f3);
+	signAndExecuteForms(b1, f1, f2, f3);
 
-    signAndExecuteForms(b3, f1, f2, f3);
+	signAndExecuteForms(b2, f1, f2, f3);
 
-    return (0);
+	signAndExecuteForms(b3, f1, f2, f3);
+
+	return (0);
 }
