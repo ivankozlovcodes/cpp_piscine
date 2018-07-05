5a1d672cb132555cefdd0d70c056c060a9e28e3d
Ivan Kozlov <ikozlov9000@gmail.com>
2018-07-04 20:40:00 -0700

    d08 - ex01: added tests

--- d08/ex01/main.cpp
+++ d08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/04 19:01:18 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/07/04 19:08:25 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/07/04 20:38:24 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,50 @@
 int
 main(void)
 {
-	Span sp(10);
+	unsigned int	n = 10;
+	Span sp(n);
 
 	sp.addNumber(10);
 	try
 	{
+		std::cout << "Trying to add the same number" << std::endl;
 		sp.addNumber(10);
-		// std::cout << sp.longestSpan() << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	sp.addNumber(1);
-	std::cout << sp.longestSpan() << std::endl;
+	
+	try
+	{
+		std::cout << "Trying to get spans with one number" << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+		std::cout << sp.shortestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	srand(time(NULL));
+	for (int i = 1; i < n; i++)
+		try
+		{
+			sp.addNumber(rand() % 100);
+		}
+		catch (const std::exception& e)
+		{
+			i--;
+		}
+	
+
+	std::cout << "Here is numbers I generated" << std::endl;
+	std::cout << sp << std::endl;
+	std::cout << "Shortest span is: " << sp.shortestSpan() << std::endl;
+	std::cout << "Longest span is: " << sp.longestSpan() << std::endl;
+
+	std::cout << "Now I will add one more number to span and die with exception. Thanks" << std::endl;
+	sp.addNumber(666);
 
 	return (0);
 }
