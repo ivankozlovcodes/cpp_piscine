1b1b01e8f5b825afdd3dba9a075b474b3fd5c464
Ivan Kozlov <ikozlov9000@gmail.com>
2018-07-06 19:10:38 -0700

    d08 - ex02: removed old implementation

--- d08/ex02/mutantstack.hpp
+++ d08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/04 21:08:04 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/07/05 22:39:47 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/07/06 19:09:40 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,72 +48,3 @@ class MutantStack : public std::stack<T>
 };
 
 #endif
-
-/* old
-
-template <typename T>
-class MutantStack : public std::stack<T>
-{
-
-	public:
-
-		MutantStack<T>(void) { }
-
-		MutantStack<T>(MutantStack<T> const &r)
-		{
-			this->_list = r._list;
-			return (*this);
-		}
-
-		~MutantStack<T>(void) { }
-
-		MutantStack &operator= (MutantStack<T> const &r)
-		{
-			*this = r;
-			return (*this);
-		}
-
-		bool		empty(void)
-		{
-			return (_list.empty());
-		}
-
-		size_t		size(void)
-		{
-			return (_list.size());
-		}
-
-		size_t		top(void)
-		{
-			return (_list.front());
-		}
-
-		void		push(T val)
-		{
-			_list.push_front(val);
-		}
-
-		void		pop(void)
-		{
-			_list.pop_front();
-		}
-
-		// typedef std::iterator<std::forward_iterator_tag, T> iterator;
-		typedef typename std::list<T>::iterator iterator;
-
-		iterator	begin()
-		{
-			return (_list.begin());
-		}
-
-		iterator end() {
-			return (_list.end());
-		}
-
-	private:
-
-		std::list<T>	_list;
-
-};
-
-*/
