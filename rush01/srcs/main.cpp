c17bc10c1781d0a868c3ccc86497b8b36936ab75
Ivan Kozlov <ikozlov9000@gmail.com>
2018-07-07 16:23:05 -0700

    rush01: added skeleton for main

--- rush01/srcs/main.cpp
+++ rush01/srcs/main.cpp
@@ -3,19 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/07 02:00:41 by ivankozlov        #+#    #+#             */
-/*   Updated: 2018/07/07 02:32:47 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2018/07/07 15:58:51 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_gkrellm.hpp"
 
+std::vector<IMonitorModule*>
+buildModules(void)
+{
+	std::vector<IMonitorModule*>		modules;
+
+	modules.push_back(new HostNameModule(" Host Name "));
+
+	return (modules);
+}
+
 int
 main(int ac, char *av[])
 {
-	static_cast<void>(ac);
-	static_cast<void>(av);
+	std::vector<IMonitorModule*>		modules;
+	// todo: parse flags
+	// todo: help command
+	// todo: errors
+	// todo: build modules
+	modules = buildModules();
+	TerminalDisplay		root(modules);
+
+	root.main();
+
 	return (0);
 }
