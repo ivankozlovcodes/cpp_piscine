b2d6b7b3dba418bd90393f97047b18704664a8ea
Ivan Kozlov <ikozlov9000@gmail.com>
2018-07-07 16:31:31 -0700

    rush01: added implementation for IMonitorDisplay

---
+++ rush01/srcs/IMonitorDisplay.cpp
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IMonitorDisplay.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/07 16:28:26 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/07/07 16:28:57 by ikozlov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "IMonitorDisplay.hpp"
+
+IMonitorDisplay::IMonitorDisplay(void) { }
+
+IMonitorDisplay::~IMonitorDisplay(void) { }
