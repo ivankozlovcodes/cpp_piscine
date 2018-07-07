04b219ba74ff658af8a13a345c5400e64f159e2b
Ivan Kozlov <ikozlov9000@gmail.com>
2018-07-07 16:22:46 -0700

    rush01: implemented host name module

---
+++ rush01/includes/HostNameModule.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HostNameModule.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/07 15:40:23 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/07/07 16:16:06 by ikozlov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HOSTNAMEMODULE_HPP
+# define HOSTNAMEMODULE_HPP
+
+# include "ft_gkrellm.hpp"
+
+class HostNameModule : public BaseModule
+{
+	public:
+
+		HostNameModule(std::string const name);
+		virtual ~HostNameModule(void);
+
+		void						tick(void);
+
+	private:
+
+		HostNameModule(void);
+		HostNameModule(HostNameModule const &obj);
+		HostNameModule	&operator=(HostNameModule const &r);
+
+};
+
+#endif
---
+++ rush01/srcs/HostNameModule.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HostNameModule.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/07 15:55:34 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/07/07 16:16:41 by ikozlov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_gkrellm.hpp"
+
+HostNameModule::HostNameModule(std::string name) : BaseModule(name)
+{
+	char		buff[_POSIX_HOST_NAME_MAX];
+
+	gethostname(buff, _POSIX_HOST_NAME_MAX);
+	_out.push_back(buff);
+	getlogin_r(buff, _POSIX_HOST_NAME_MAX);
+	_out.push_back(buff);
+}
