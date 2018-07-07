c1366a1c527cb500901309782713719f6dc6d638
Ivan Kozlov <ikozlov9000@gmail.com>
2018-07-07 16:22:54 -0700

    rush01:  updated main header

--- rush01/includes/ft_gkrellm.hpp
+++ rush01/includes/ft_gkrellm.hpp
@@ -3,19 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_gkrellm.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/07 01:58:23 by ivankozlov        #+#    #+#             */
-/*   Updated: 2018/07/07 02:32:00 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2018/07/07 16:16:30 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_GKRELLM_HPP
 # define FT_GKRELLM_HPP
 
+# include <ncurses.h>
+# include <unistd.h>
+# include <limits.h>
 # include <string>
+# include <vector>
 
 # include "IMonitorModule.hpp"
 # include "IMonitorDisplay.hpp"
+
+# include "TerminalDisplay.hpp"
+
+# include "BaseModule.hpp"
+# include "HostNameModule.hpp"
 
 #endif
