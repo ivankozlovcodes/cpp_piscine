da7fac85ef406af851a8226134e91a92fa29288b
Ivan Kozlov <ikozlov9000@gmail.com>
2018-07-07 21:04:18 -0700

    rush01: upd header

--- rush01/includes/ft_gkrellm.hpp
+++ rush01/includes/ft_gkrellm.hpp
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/07 01:58:23 by ivankozlov        #+#    #+#             */
-/*   Updated: 2018/07/07 20:26:21 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/07/07 20:41:58 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@
 # include <limits.h>
 # include <curses.h>
 # include <iostream>
+#include <sys/utsname.h>
 // # include <ncurses.h>
 
 # include "IMonitorModule.hpp"
 # include "IMonitorDisplay.hpp"
 
 # include "BaseModule.hpp"
+# include "OSInfoModule.hpp"
 # include "HostNameModule.hpp"
 
 # include "TerminalDisplay.hpp"
