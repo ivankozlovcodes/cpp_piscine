08fdede232768575ed687b61b729d263c6f2555c
Ivan Kozlov <ikozlov9000@gmail.com>
2018-07-07 20:35:30 -0700

    rush01: implemented basic printing for one module

--- rush01/includes/TerminalDisplay.hpp
+++ rush01/includes/TerminalDisplay.hpp
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/07 15:45:31 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/07/07 15:49:58 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/07/07 20:28:58 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ class TerminalDisplay : public IMonitorDisplay
 		void		main(void);
 		void		refresh(void);
 		void		render(void);
+		void		fillSpace(char *str);
 
 	private:
 
@@ -33,7 +34,22 @@ class TerminalDisplay : public IMonitorDisplay
 		TerminalDisplay(TerminalDisplay const &obj);
 		TerminalDisplay	&operator=(TerminalDisplay const &r);
 
+		void		printFrame(void);
+		void		printTextCenter(std::string text);
+		void		printModule(BaseModule *module);
+		const char	*buildString(const char *s, std::string head,\
+			std::string tail, int len);
+
 		std::vector<IMonitorModule*>	_modules;
+		int								_cols;
+		int								_rows;
+		int								_x, _y;
+		int								_mid;
+		std::string						_name;
+		WINDOW							*_w;
+
+		std::string						_filler;
+		std::string						_separator;
 };
 
 #endif
--- rush01/srcs/TerminalDisplay.cpp
+++ rush01/srcs/TerminalDisplay.cpp
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/07 16:25:52 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/07/07 16:28:00 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/07/07 20:34:20 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,136 @@
 
 TerminalDisplay::TerminalDisplay(std::vector<IMonitorModule*> modules) :
 	IMonitorDisplay(),
-	_modules(modules) { }
+	_name("FT_GKRELLM"),
+	_modules(modules)
+{
+	initscr(); // initialize screen
+	cbreak();
+	noecho(); // stop displaying user input
+	nonl();
+	intrflush(stdscr, FALSE);
+	keypad(stdscr, TRUE);
+	curs_set(0);
+	keypad(stdscr, TRUE);
+	nodelay(stdscr, TRUE);
 
-TerminalDisplay::~TerminalDisplay(void) { }
+	getmaxyx(stdscr, _rows, _cols);
+	_w = stdscr;
+	_mid = _cols / 2;
+	_filler = buildString("-", "||", "||", _cols);
+	_separator = buildString("=", "||", "||", _cols);
+}
+
+TerminalDisplay::~TerminalDisplay(void) 
+{
+	endwin(); // close window
+}
 
 void
 TerminalDisplay::main(void)
 {
-
+	while(1)
+	{
+		refresh();
+		render();
+	}
 }
 
 void
 TerminalDisplay::refresh(void)
 {
-	
+	std::vector<IMonitorModule*>::iterator it;
+
+	_x = -1;
+	_y = -1;
+	for (it = _modules.begin(); it != _modules.end(); it++)
+		(**it).tick();
+}
+
+void					TerminalDisplay::fillSpace(char *str)
+{
+	while (this->_y < this->_cols)
+	{
+		mvprintw(this->_x, this->_y, str);
+		this->_y = this->_y + 1;
+	}
+	this->_x = this->_x + 1;
+	this->_y = -1;
+}
+
+const char
+*TerminalDisplay::buildString(const char *s, std::string head,\
+	std::string tail, int len)
+{
+	std::string		mid;
+	int				s_len = strlen(s);
+
+	len = len - head.length() - tail.length();
+	for (int i = 0; i < len; i += s_len)
+		mid.append(s);
+	mid.erase(len, mid.length());
+	head.append(mid);
+	head.append(tail);
+	return (head.c_str());
+}
+
+void
+TerminalDisplay::printFrame(void)
+{
+	mvprintw(_x++, _y, buildString("= ", "  /", "\\  ", _cols));
+	mvprintw(_x++, _y, buildString("= ", " /", "\\ ", _cols));
+	for (int i = _x; i < _rows - 2; i++)
+	{
+		mvprintw(i, _y, "||");
+		mvprintw(i, _cols - 2, "||");
+	}
+	mvprintw(_rows - 2, _y, buildString("= ", " \\", "/ ", _cols));
+	mvprintw(_rows - 1, _y, buildString("= ", "  \\", "/  ", _cols));
+}
+
+void
+TerminalDisplay::printTextCenter(std::string text)
+{
+	mvprintw(_x, _mid - text.length() / 2, text.c_str());
+}
+
+void
+TerminalDisplay::printModule(BaseModule *module)
+{
+	std::vector<std::string>::iterator		it;
+	std::vector<std::string>				info = module->getOutput();
+
+	mvprintw(_x++, _y, _separator.c_str());
+	mvprintw(_x++, _y, _filler.c_str());
+	mvprintw(_x, _y, _filler.c_str());
+	printTextCenter(module->getName());
+	_x++;
+	mvprintw(_x++, _y, _filler.c_str());
+	for (it = info.begin(); it != info.end(); it++)
+	{
+		mvprintw(_x, _y, _filler.c_str());
+		printTextCenter(*it);
+		_x++;
+	}
+	mvprintw(_x++, _y, _separator.c_str());
 }
 
 void
 TerminalDisplay::render(void)
 {
-	
+	std::vector<IMonitorModule*>::iterator	it;
+	IMonitorModule							*current;
+
+	_x = 0; _y = 0;
+	printFrame();
+	mvprintw(_x++, _y, _separator.c_str());
+	mvprintw(_x++, _y, _filler.c_str());
+	mvprintw(_x, _y, _filler.c_str());
+	printTextCenter(_name);
+	_x++;
+	mvprintw(_x++, _y, _filler.c_str());
+	mvprintw(_x++, _y, _separator.c_str());
+	for (it = _modules.begin(); it != _modules.end(); it++)
+		printModule(dynamic_cast<BaseModule*>(*it));
+	wrefresh(_w);
 }
